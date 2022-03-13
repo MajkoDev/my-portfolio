4f9bd425c9d6dc1e9011c60d12eb20e93c0b81af
Marian <hellomariandev@gmail.com>
2022-03-13 23:13:55 +0100

    components advanced

--- components/sections/AboutSection.jsx
+++ components/sections/AboutSection.jsx
@@ -1,34 +1,66 @@
-import { Container, Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 const AboutSection = () => {
   return (
     <Stack h='full' w='full'>
-
-      <Stack alignItems={{ base: "center", md: "start" }} mb={{base: '4', md: '12'}}>
+      <Stack
+        mb={{ base: "4", md: "10" }}
+        alignItems={{ base: "center", md: "start" }}>
         <Heading fontSize='5xl'>O mne.</Heading>
         <Text fontSize='lg'>Čo som schopný o sebe prezradiť?</Text>
       </Stack>
 
-      <Flex direction={{ base: "column", md: "row" }}>
-
-        <Container border='4px' borderColor='gray.200' borderRadius={12} p={3} m={3} > 
-          <Heading fontSize='3xl' mb={3}>kto som?</Heading>
-          <Text fontSize='md'>
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        align={{ base: "center", md: "start" }}
+        justify='center'
+        w={{ base: "100%", md: "100%" }}>
+        <Container
+          maxW='sm'
+          border='4px'
+          borderColor='gray.200'
+          borderRadius={12}
+          p={3}
+          m={1}>
+          <Heading fontSize='2xl' mb={2}>
+            kto som?
+          </Heading>
+          <Text fontSize='sm'>
             venujem sa web vývoju. <br />
             tvorím zaujímavé a živé veci na internet. <br />
             chcem preskočiť od záujmu k povolaniu.
           </Text>
         </Container>
 
-        <Container border='4px' borderColor='gray.200' borderRadius={12} p={3} m={3}>
-          <Heading fontSize='3xl'>čo momentálne?</Heading>
-          <Text fontSize='md'>
+        <Container
+          maxW='sm'
+          border='4px'
+          borderColor='gray.200'
+          borderRadius={12}
+          p={3}
+          m={1}>
+          <Heading fontSize='2xl' mb={2}>
+            čo momentálne?
+          </Heading>
+          <Text fontSize='sm'>
             momentálne tvorím projekty pre môj Github. <br /> experimentujem s
             reactom and prehlbujem znalosti javascriptu. <br /> tvorím stránky,
             ktoré chcem ponúknuť známym pre využitie.
           </Text>
         </Container>
       </Flex>
+
+      <Stack w='full' align={{ base: "center", md: "end" }}>
+        <Link mt={8}>Zisti viac o mne</Link>
+      </Stack>
     </Stack>
   );
 };
--- components/sections/SkillsSection.jsx
+++ components/sections/SkillsSection.jsx
@@ -1,11 +1,32 @@
-import { Heading, Stack } from "@chakra-ui/react"
+import { Heading, Text, Stack, Flex, Container } from "@chakra-ui/react";
 
 const SkillsSection = () => {
-    return(
-        <Stack>
-            <Heading>Skills Section</Heading>
-        </Stack>
-    )
-}
+  return (
+    <Stack h='full' w='full'>
+      <Stack
+        mb={{ base: "4", md: "10" }}
+        alignItems={{ base: "center", md: "start" }}>
+        <Heading fontSize='3xl'>moje schopnosti.</Heading>
+        <Text fontSize='lg'>vedomosti, skusenosti</Text>
+      </Stack>
 
-export default SkillsSection
+      <Flex wrap='auto'
+        direction={{ base: "column", md: "row" }}
+        align={{ base: "center", md: "start" }}
+        justify='center'
+        w={{ base: "100%", md: "100%" }}>
+
+            <Container bg='blue.100' w={356} h={125} m={2}>
+            </Container>
+
+            <Container bg='blue.100' w={356} h='full' m={2}>
+            </Container>
+
+            <Container bg='blue.100' w={356} h={125} m={2}>
+            </Container>
+        </Flex>
+    </Stack>
+  );
+};
+
+export default SkillsSection;
