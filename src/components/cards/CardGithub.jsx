d6e8514321f26f69338b8aaf25bd027d805ef717
Marian <hellomariandev@gmail.com>
2022-03-19 00:42:37 +0100

    updated CardGithub.jsx

--- src/components/cards/CardGithub.jsx
+++ src/components/cards/CardGithub.jsx
@@ -6,8 +6,10 @@ import {
   Flex,
   Spacer,
   Box,
+  HStack,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
+import { FiCodesandbox } from "react-icons/fi";
 
 const CardGithub = () => {
   return (
@@ -22,18 +24,23 @@ const CardGithub = () => {
         <Heading size='sm'>Note Taking App</Heading>
         <Spacer />
 
-        <NextLink href='https://github.com/MajkoDev' passHref>
-          <Link fontWeight='600' fontSize='xs'>
-            CodeSandBox
-          </Link>
-        </NextLink>
+          <NextLink href='/' passHref>
+            <Link fontWeight='600' fontSize='xs'>
+              <HStack>
+                <FiCodesandbox m={2} />
+                <Text>CodeSandBox</Text>
+              </HStack>
+            </Link>
+          </NextLink>
       </Flex>
 
-      <Text fontSize='xs' mb={2}>
+      <Text fontSize='xs' mb={6}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa amet
         voluptatem libero.
       </Text>
-      <Text fontSize='xs'>React Tailwind Firebase</Text>
+      <Text fontSize='xs' fontWeight='semibold'>
+        React Tailwind Firebase
+      </Text>
     </Container>
   );
 };
