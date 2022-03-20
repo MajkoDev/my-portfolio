c7b9c21aa0b1c29d37d0013825db03bce836f26a
Marian <hellomariandev@gmail.com>
2022-03-20 19:04:07 +0100

    update hero section

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 // Sections
-import HeroSection from "../components/sections/HeroSection";
-import AboutSection from "../components/sections/AboutSection";
-import SkillsSection from "../components/sections/SkillsSection";
+import SectionHero from "../src/sections/SectionHero"
 
 export default function Home() {
   return (
     <>
-      <HeroSection />
-      <AboutSection />
-      <SkillsSection />
+      <SectionHero /> 
     </>
   );
 }
--- src/sections/SectionHero.jsx
+++ src/sections/SectionHero.jsx
@@ -1,11 +1,33 @@
-import { Stack } from "@chakra-ui/react";
+import { Heading, Image, Link, Stack, Text } from "@chakra-ui/react";
 
 const SectionHero = () => {
-    return (
-        <Stack h='full' w='full'>
-            Enter
-        </Stack>
-    );
-}
+  return (
+    <Stack
+      h='full'
+      w='full'
+      py={{ base: "12", md: "24" }}
+      direction={{ base: "column", md: "row" }}
+      alignItems='center'>
+      <Stack my='10' direction='column'>
+        <Text fontSize='4xl' mb='-5' pl={1}>
+          Volám sa,{" "}
+        </Text>
+        <Heading fontSize='8xl'>Marian.</Heading>
+        <Text as='h3' pl={1}>
+          <strong> front-end (samouk) developer </strong> zo západného
+          Slovenska. <br /> nadšenec pre <strong>React</strong>, príležitostne
+          sa venujem dizajnu.
+        </Text>
+        <Link pl={1}>hellomariandev@gmail.com</Link>
+      </Stack>
+      <Image
+        src='https://bit.ly/dan-abramov'
+        boxSize='250px'
+        borderRadius='full'
+        alt='Profile'
+      />
+    </Stack>
+  );
+};
 
 export default SectionHero;
