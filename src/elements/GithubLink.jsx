c3b6512fc457e3e007eca4775752e14dbdb1c2b8
Marian <hellomariandev@gmail.com>
2022-04-02 23:16:09 +0200

    update github link

--- src/elements/GithubLink.jsx
+++ src/elements/GithubLink.jsx
@@ -1,21 +1,28 @@
-import { Heading, Link, HStack, Box } from "@chakra-ui/react";
-import NextLink from "next/link";
+import { Heading, HStack, Box, Button } from "@chakra-ui/react";
+import Link from "next/link";
 import { ImGithub } from "react-icons/im";
 
 export default function GithubLink() {
   return (
     <Box mt={5}>
-      <NextLink href='/' passHref>
-        <Link>
-          <HStack>
-            <Heading fontSize={{ base: "lg", md: "2xl" }} fontWeight={300}>
-              navštív môj{" "}
-            </Heading>
-            <Heading fontSize={{ base: "lg", md: "2xl" }}>GitHub</Heading>
-            <ImGithub size='22' />
-          </HStack>
-        </Link>
-      </NextLink>
+      <Link href='https://github.com/MajkoDev' passHref>
+        <a target='_blank' rel='noreferrer'>
+          <Button variant='ghost'>
+            <HStack>
+              
+              <Heading fontSize={{ base: "lg", md: "2xl" }} fontWeight={300}>
+                navštív môj{" "}
+              </Heading>
+
+              <Heading fontSize={{ base: "lg", md: "2xl" }}>GitHub</Heading>
+
+              <Box ml='1'>
+                <ImGithub size='22' />
+              </Box>
+            </HStack>
+          </Button>
+        </a>
+      </Link>
     </Box>
   );
 }
