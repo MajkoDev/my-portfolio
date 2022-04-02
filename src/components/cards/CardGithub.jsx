2decef67a7b92001ee1abc24e2324e299b2116f9
Marian <hellomariandev@gmail.com>
2022-04-02 23:39:09 +0200

    better github design

--- src/components/cards/CardGithub.jsx
+++ src/components/cards/CardGithub.jsx
@@ -3,34 +3,36 @@ import {
   Heading,
   Link,
   Text,
+  Button,
   Flex,
+  Box,
   Spacer,
   HStack,
-  WrapItem, useColorModeValue
+  WrapItem,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { FiCodesandbox, FiGithub } from "react-icons/fi";
 
-
 const CardGithub = () => {
-
-  const colorTitle = useColorModeValue("gray.700", "gray.200");
+  const colorTitle = useColorModeValue("gray.800", "gray.200");
 
   return (
     <WrapItem>
-      <Container
+      <Box
         border='1px'
         borderColor='gray.500'
         color={colorTitle}
         rounded='md'
         p='3'
-        maxW='320px'>
+        minW='280px'
+        maxW='340px'>
         <Flex justify='center' align='center' mb='2' mr='2'>
           <Heading size='sm'>Note Taking App</Heading>
           <Spacer />
 
           <NextLink href='/' passHref>
-            <Link fontWeight='600' fontSize='xs' >
+            <Link fontWeight='600' fontSize='xs'>
               <HStack>
                 <FiCodesandbox m='2' />
                 <Text>CodeSandBox</Text>
@@ -49,13 +51,20 @@ const CardGithub = () => {
             React Tailwind Firebase
           </Text>
           <Spacer />
-          <NextLink href='/' passHref>
-            <Link fontWeight='600' fontSize='md'>
-              <FiGithub />{" "}
-            </Link>
-          </NextLink>
+
+          <Button
+            variant='ghost'
+            borderRadius='60px'
+            size='sm'
+            borderColor='gray.600'>
+            <NextLink href='/' passHref>
+              <Link fontWeight='600' fontSize='md'>
+                <FiGithub />{" "}
+              </Link>
+            </NextLink>
+          </Button>
         </HStack>
-      </Container>
+      </Box>
     </WrapItem>
   );
 };
--- src/sections/SectionGithub.jsx
+++ src/sections/SectionGithub.jsx
@@ -1,4 +1,12 @@
-import { Center, Container, Link, Stack, Text, Wrap } from "@chakra-ui/react";
+import {
+  Center,
+  Container,
+  Box,
+  Link,
+  Stack,
+  Text,
+  Wrap,
+} from "@chakra-ui/react";
 
 // Components
 import CardGithub from "../components/cards/CardGithub";
@@ -7,46 +15,16 @@ import CardGithub from "../components/cards/CardGithub";
 import SectionTitle from "../elements/SectionTitle";
 import GithubLink from "../elements/GithubLink";
 
-// Apollo Client
-import {
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-  gql,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-
-const SectionGithub = ({ pinnedItems }) => {
-  console.log(pinnedItems);
-
-  const listRepositories =
-    pinnedItems &&
-    pinnedItems.map((item) => (
-      <Link key={item.id} src={item.url}>
-        <Text>{item.name}</Text>
-      </Link>
-    ));
-
+const SectionGithub = () => {
   return (
     <Stack h='full' w='full'>
       <SectionTitle
         title='github.'
         subtitle='Posledné projekty a repozitoriá.'
       />
-
-      <Container m={5}>
-        {pinnedItems &&
-          pinnedItems.data.map((item) => {
-            <Link key={item.id} src={item.url}>
-              <Text>{item.name}</Text>
-            </Link>;
-          })}
-      </Container>
-
-
+      <Stack pl={{base: '0', lg: '100px'}}>
 
       <Wrap spacing='20px' justify='center'>
-        <CardGithub />
         <CardGithub />
         <CardGithub />
         <CardGithub />
@@ -56,62 +34,9 @@ const SectionGithub = ({ pinnedItems }) => {
       <Center>
         <GithubLink />
       </Center>
+      </Stack>
     </Stack>
   );
 };
 
 export default SectionGithub;
-
-export async function getStaticProps() {
-  const httpLink = createHttpLink({
-    uri: "https://api.github.com/graphql",
-  });
-
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
-  const { data } = await client.query({
-    query: gql`
-      {
-        user(login: "colbyfayock") {
-          pinnedItems(first: 6) {
-            totalCount
-            edges {
-              node {
-                ... on Repository {
-                  name
-                  id
-                  url
-                  stargazers {
-                    totalCount
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `,
-  });
-
-  const { user } = data;
-
-  const pinnedItems = user.pinnedItems.edges.map((edge) => edge.node);
-
-  return {
-    props: {
-      pinnedItems,
-    },
-  };
-}
