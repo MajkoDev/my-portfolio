c0f67f669b4284bcc43585754dac413e2a150e9f
Marian <hellomariandev@gmail.com>
2022-04-09 19:33:12 +0200

    correction on project card

--- src/components/cards/ProjectCardTwo.jsx
+++ src/components/cards/ProjectCardTwo.jsx
@@ -1,49 +1,50 @@
 import {
-    Container,
-    Box,
-    Heading,
-    Stack,
-    Text,
-    Flex,
-    Link,
-  } from "@chakra-ui/react";
-  import Image from "next/image";
-  import mac from "../../../public/images/mac.jpg";
-  
-  const ProjectCardTwo = () => {
-    return (
-      <>
-  
-        <Box
-          h={{ base: "400px", md: "264px" }}
-          w={{ base: "md", md: "2xl" }}
-          p='0'
-          pb='8'
-          rounded='md'
-          boxShadow='2xl'
-          overflow='hidden'>
-  
-          <Flex direction={{ base: "column", md: "row" }}>
-  
-            <Box
-              w={{ base: "full", md: "45%" }}
-              h={{ base: "240", md: "264px" }}
-              position='relative'>
-                
-              <Image
-                alt='Project Image'
-                src={mac}
-                layout='fill'
-                objectFit='cover'
-                quality={100}
-              />
-            </Box>
-  
-            <Box
-              pl='6'
-              pt={{ base: "2", md: "6" }}
-              w={{ base: "full", md: "55%" }}
-              h={{ base: "160", md: "264px" }}>
+  VStack,
+  Box,
+  Heading,
+  Spacer,
+  Stack,
+  Text,
+  Flex,
+  Link,
+  HStack,
+} from "@chakra-ui/react";
+import Image from "next/image";
+import mac from "../../../public/images/mac.jpg";
+
+const ProjectCardTwo = () => {
+  return (
+    <>
+      <Box
+        h={{ base: "400px", md: "264px" }}
+        w={{ base: "md", md: "2xl" }}
+        rounded='md'
+        boxShadow='2xl'
+        overflow='hidden'>
+        <Flex direction={{ base: "column", md: "row" }}>
+          <Box
+            w={{ base: "full", md: "45%" }}
+            h={{ base: "240", md: "264px" }}
+            position='relative'>
+            <Image
+              alt='Project Image'
+              src={mac}
+              layout='fill'
+              objectFit='cover'
+              quality={100}
+            />
+          </Box>
+
+          <Box
+            px='4'
+            py={{ base: "2", md: "6" }}
+            w={{ base: "full", md: "55%" }}
+            h={{ base: "160", md: "264px" }}>
+            <Flex
+              direction='column'
+              h='full'
+              justify='space-between'
+              align='stretch'>
               <Text
                 color='green.600'
                 mt='1'
@@ -55,24 +56,32 @@ import {
               <Heading mt='2' fontSize={{ base: "lg", md: "xl" }}>
                 MajkoDev_ front-end developer
               </Heading>
+
               <Text
                 fontSize={{ base: "xs", md: "sm" }}
                 fontWeight={400}
                 mt='1'
                 mr='4'>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-                erat, sed diam voluptua.
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+                diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua.
               </Text>
-              <Link fontSize={{ base: "xs", md: "sm" }} fontWeight={800} mt='1'>
-                View Website
-              </Link>
-            </Box>
-          </Flex>
-        </Box>
-      </>
-    );
-  };
-  
-  export default ProjectCardTwo;
-  
+              <Spacer />
+
+              <HStack justify='end'>
+                <Link
+                  fontSize={{ base: "sm", md: "sm" }}
+                  fontWeight={600}
+                  m='2'>
+                  View Website
+                </Link>
+              </HStack>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default ProjectCardTwo;
